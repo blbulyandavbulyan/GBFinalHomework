7fbaa29715e55598c87818600afdff1439e06901
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-07 12:45:35 +0400

    initial commit

---
+++ src/main/java/com/blbulyandavbulyan/gbfinalhoomework/GbFinalHoomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.gbfinalhoomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbFinalHoomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbFinalHoomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blbulyandavbulyan/gbfinalhoomework/GbFinalHoomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.gbfinalhoomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbFinalHoomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
