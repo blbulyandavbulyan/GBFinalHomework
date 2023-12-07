336181a2d93f7282b6b3bbad76518340eddd537c
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-07 14:50:47 +0400

    main class was renamed, misprint fixed

--- src/main/java/com/blbulyandavbulyan/gbfinalhoomework/GbFinalHomeworkApplication.java
+++ src/main/java/com/blbulyandavbulyan/gbfinalhoomework/GbFinalHomeworkApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GbFinalHoomeworkApplication {
+public class GbFinalHomeworkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GbFinalHoomeworkApplication.class, args);
+        SpringApplication.run(GbFinalHomeworkApplication.class, args);
     }
-
 }
